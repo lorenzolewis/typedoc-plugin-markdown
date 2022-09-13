3346d5888130e9ab8ed3214239e91c9979a7cec4
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-09-13 16:06:40 +0100

    Add handling for `@since` and `@history` tags

--- packages/typedoc-plugin-markdown/src/partials/comment.ts
+++ packages/typedoc-plugin-markdown/src/partials/comment.ts
@@ -1,6 +1,7 @@
 import { Comment } from 'typedoc';
 import { MarkdownThemeRenderContext } from '../theme-context';
 import { camelToTitleCase } from '../utils';
+import { bold } from '../els';
 
 export function comment(context: MarkdownThemeRenderContext, comment: Comment) {
   const md: string[] = [];
@@ -12,12 +13,27 @@ export function comment(context: MarkdownThemeRenderContext, comment: Comment) {
   if (comment.blockTags?.length) {
     const tags = comment.blockTags
       .filter((tag) => tag.tag !== '@returns')
-      .map(
-        (tag) =>
-          `**${camelToTitleCase(
-            tag.tag.substring(1),
-          )}**\n\n${context.partials.commentParts(tag.content)}`,
-      );
+      .map((tag) => {
+        switch (tag.tag) {
+          case '@since':
+            return `${bold(
+              camelToTitleCase(tag.tag.substring(1)),
+            )}: ${context.partials.commentParts(tag.content)}`;
+          case '@history':
+            const history: string[] = [];
+            history.push('<details>');
+            history.push(
+              `<summary>${camelToTitleCase(tag.tag.substring(1))}</summary>`,
+            );
+            history.push(context.partials.commentParts(tag.content));
+            history.push('</details>');
+            return history.join('\n\n');
+          default:
+            return `${bold(
+              camelToTitleCase(tag.tag.substring(1)),
+            )}\n\n${context.partials.commentParts(tag.content)}`;
+        }
+      });
     md.push(tags.join('\n\n'));
   }
 
